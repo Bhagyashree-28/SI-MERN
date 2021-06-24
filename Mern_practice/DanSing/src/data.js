61aa7a41daee29ca7b8ea8426f3d48706f666c9b
Bhagyashree-28 <bhagyashree.glsica17@gmail.com>
2021-06-24 21:36:43 +0530

    upload

--- Mern_practice/DanSing/src/data.js
+++ Mern_practice/DanSing/src/data.js
@@ -1,29 +1,29 @@
 const people=[
     {
         id:1,
-        image:'https://i.pinimg.com/564x/48/dc/b8/48dcb8b45bc1772bffec46b1759734c8.jpg',
-        name:'Byun baekhyun',
+        image:'https://img.freepik.com/free-vector/cute-boy-standing-position-showing-thumb_96037-450.jpg?size=626&ext=jpg',
+        name:'rock',
         title:'Singer,dancer',
 
     },
     {
         id:2,
-        image:'https://64.media.tumblr.com/6930ca08c7039e841f97995498867050/tumblr_p76ga7M29i1riav2to1_500.jpg',
-        name:'Park chanyeol',
+        image:'https://img.freepik.com/free-vector/cute-boy-standing-position-showing-thumb_96037-450.jpg?size=626&ext=jpg',
+        name:'andy',
         title:'Singer,dancer,rapper',
 
     },
     {
         id:3,
-        image:'https://i.pinimg.com/564x/51/0a/35/510a350bf0e91192c0e1864dbf0e13c8.jpg',
-        name:'Chen',
+        image:'https://img.freepik.com/free-vector/cute-boy-standing-position-showing-thumb_96037-450.jpg?size=626&ext=jpg',
+        name:'sujan',
         title:'Singer,dancer',
 
     },
     {
         id:4,
-        image:'https://i.pinimg.com/236x/5a/67/3c/5a673c788cb0f16ea51df8d34b84d544.jpg',
-        name:'Ooh sehun',
+        image:'https://img.freepik.com/free-vector/cute-boy-standing-position-showing-thumb_96037-450.jpg?size=626&ext=jpg',
+        name:'sehun',
         title:'Singer,dancer',
 
     },
